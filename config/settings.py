5aac67204dfa6e6c968cf361eb96dfbcd4fe8721
admin <malvandi.py@gmail.com>
2024-02-12 19:16:18 +0330

    add customuser to project

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party Apps
+
+    # Local Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Account config
+AUTH_USER_MODEL = 'accounts.CustomUser'
